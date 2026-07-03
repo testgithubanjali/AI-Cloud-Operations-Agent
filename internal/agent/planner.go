4459f993578ce4562496abfe27d17ba1c8485f42
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-03 23:55:14 +0530

    update: update planner.go

--- internal/agent/planner.go
+++ internal/agent/planner.go
@@ -9,24 +9,45 @@ import (
 
 func Process(userMessage string) (string, error) {
 
-	// Ask Gemini what tool should be used
+	// Step 1: Ask Gemini which tool to use
 	response, err := llm.Ask(userMessage)
 	if err != nil {
 		return "", err
 	}
 
-	// Try to parse Gemini's response as JSON
+	// Step 2: Convert Gemini's JSON response into a Go struct
 	var toolCall tools.ToolCall
 
 	err = json.Unmarshal([]byte(response), &toolCall)
 
-	// If it's not JSON, Gemini answered normally.
+	// If Gemini returned normal text instead of JSON,
+	// just return the response.
 	if err != nil {
 		return response, nil
 	}
 
-	// Execute the requested tool
-	result := tools.ExecuteTool(toolCall)
+	// Step 3: Execute the requested tool
+	toolResult := tools.ExecuteTool(toolCall)
 
-	return result, nil
+	// Step 4: Ask Gemini to explain the tool output
+	finalPrompt := `
+You are an AI Kubernetes SRE assistant.
+
+The user asked:
+
+` + userMessage + `
+
+The tool returned:
+
+` + toolResult + `
+
+Generate a clear, helpful and concise answer for the user.
+`
+
+	finalAnswer, err := llm.Ask(finalPrompt)
+	if err != nil {
+		return "", err
+	}
+
+	return finalAnswer, nil
 }
