837d64892bc0f15fededd25cb97320d07a2ad66f
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-04 18:31:49 +0530

    update: updated some parts of the code

--- internal/tools/executor.go
+++ internal/tools/executor.go
@@ -5,18 +5,46 @@ func ExecuteTool(call ToolCall) string {
 	switch call.Tool {
 
 	case "get_pods":
-		return `postgres-0
-postgres-1
-redis-0`
+
+		pods, err := GetPods("default")
+		if err != nil {
+			return err.Error()
+		}
+
+		result := ""
+
+		for _, pod := range pods {
+			result += pod + "\n"
+		}
+
+		return result
 
 	case "describe_pod":
-		return `Name: postgres-0
-Status: Running
-Ready: True
-Restarts: 0`
+
+		result, err := DescribePod("default", call.Pod)
+		if err != nil {
+			return err.Error()
+		}
+
+		return result
 
 	case "get_logs":
-		return `Application started successfully`
+
+		result, err := GetLogs("default", call.Pod)
+		if err != nil {
+			return err.Error()
+		}
+
+		return result
+
+	case "get_metrics":
+
+		result, err := GetPodMetrics("default")
+		if err != nil {
+			return err.Error()
+		}
+
+		return result
 
 	default:
 		return "Unknown tool"
