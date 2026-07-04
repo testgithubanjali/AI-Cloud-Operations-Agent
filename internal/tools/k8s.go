fe333927920c3cd0fde5f39a841691eafdf2d2b5
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-04 13:57:03 +0530

    add: added code inside k8s.go

---
+++ internal/tools/k8s.go
@@ -0,0 +1,31 @@
+package tools
+
+import (
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func NewClient() (*kubernetes.Clientset, error) {
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	kubeconfig := filepath.Join(home, ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
