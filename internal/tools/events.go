f38eabe48f9bf3a68eee1c458a67f510b11c57a7
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-04 17:14:11 +0530

    update: updated some parts of the code

--- internal/tools/events.go
+++ internal/tools/events.go
@@ -2,6 +2,7 @@ package tools
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -22,12 +23,29 @@ func GetEvents(namespace string) (string, error) {
 		return "", err
 	}
 
+	// Debugging
+	fmt.Println("===================================")
+	fmt.Println("Namespace:", namespace)
+	fmt.Println("Number of Events:", len(events.Items))
+	fmt.Println("===================================")
+
 	var result strings.Builder
+
+	if len(events.Items) == 0 {
+		return "No events found.", nil
+	}
 
 	for _, event := range events.Items {
 
+		fmt.Println("Reason :", event.Reason)
+		fmt.Println("Message:", event.Message)
+		fmt.Println("-----------------------------------")
+
+		result.WriteString("Reason: ")
 		result.WriteString(event.Reason)
-		result.WriteString(": ")
+		result.WriteString("\n")
+
+		result.WriteString("Message: ")
 		result.WriteString(event.Message)
 		result.WriteString("\n\n")
 	}
