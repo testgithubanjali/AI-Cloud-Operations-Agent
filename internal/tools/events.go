e03176816d593b043c6c7a6f25017e4489e50058
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-04 17:34:19 +0530

    update: updated some parts of the code

--- internal/tools/events.go
+++ internal/tools/events.go
@@ -2,13 +2,12 @@ package tools
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func GetEvents(namespace string) (string, error) {
+func GetPodEvents(namespace, podName string) (string, error) {
 
 	client, err := NewClient()
 	if err != nil {
@@ -17,30 +16,21 @@ func GetEvents(namespace string) (string, error) {
 
 	events, err := client.CoreV1().
 		Events(namespace).
-		List(context.Background(), metav1.ListOptions{})
+		List(context.Background(), metav1.ListOptions{
+			FieldSelector: "involvedObject.kind=Pod,involvedObject.name=" + podName,
+		})
 
 	if err != nil {
 		return "", err
 	}
 
-	// Debugging
-	fmt.Println("===================================")
-	fmt.Println("Namespace:", namespace)
-	fmt.Println("Number of Events:", len(events.Items))
-	fmt.Println("===================================")
+	if len(events.Items) == 0 {
+		return "No events found for pod " + podName, nil
+	}
 
 	var result strings.Builder
 
-	if len(events.Items) == 0 {
-		return "No events found.", nil
-	}
-
 	for _, event := range events.Items {
-
-		fmt.Println("Reason :", event.Reason)
-		fmt.Println("Message:", event.Message)
-		fmt.Println("-----------------------------------")
-
 		result.WriteString("Reason: ")
 		result.WriteString(event.Reason)
 		result.WriteString("\n")
